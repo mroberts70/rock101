f70b133be32ca43a12000a42f58376027595c933
Mike Roberts <mroberts@endicott.edu>
2023-05-06 16:34:15 -0400

    return to top button added
    return to top button added, plus some additional formatting and videos, etc.

--- js/scripts.js
+++ js/scripts.js
@@ -47,6 +47,23 @@ function showArtist(selectedArtist) {
 
 }
 
+// When the user scrolls down 800px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+         
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    document.getElementById("totopbutton").style.display = "block";
+  } else {
+    document.getElementById("totopbutton").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 function currentYear() {
     let d = new Date();
     document.getElementById('copyright').innerHTML = d.getFullYear();
